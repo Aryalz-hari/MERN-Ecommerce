7b7744744f178a65ef818c3afc64741e619d25dc
Hari Aryal <117088467+Aryalz-hari@users.noreply.github.com>
2025-12-20 09:28:52 +0545

    Update CORS origin to production URL

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ mongoose
 
   const PORT = process.env.PORT || 5000;
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://frontend-ymwa.onrender.com',
     methods:['GET','POST','DELETE','PUT'],
     allowedHeaders:[
         "Content-Type",
